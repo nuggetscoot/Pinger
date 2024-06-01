ce13f16d4f63d020aa3a5f7062ae80641f622d3a
nuggetscoot <142285926+nuggetscoot@users.noreply.github.com>
2024-05-31 20:33:29 -0500

    pinger project

--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.NetworkInformation;
+using System.Text;
 
+// Pinging Google DNS Server 4.2.2.2
+
+Ping pingSender = new Ping();
+PingOptions options = new PingOptions();
+
+options.DontFragment = true;
+
+string data = "Learn to code";
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+int timeout = 120;
+string address = "4.2.2.2";
+PingReply reply = pingSender.Send(address, timeout, buffer, options);
+
+if (reply.Status == IPStatus.Success)
+{
+    Console.WriteLine("Response: {0}", reply.Status.ToString());
+    Console.WriteLine("RoundTrip: {0}", reply.RoundtripTime);
+    Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+    Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+}
